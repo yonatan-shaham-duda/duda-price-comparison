5c09c0867e0699604ddf893cb4c04b111a881a7a
YonatanShaham <yjonas83@gmail.com>
2023-02-06 13:11:32 +0200

    get products from one site

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,7 +1,28 @@
+const axios = require("axios");
+const user = "76d14a7db3";
+const password = "jCGuUNhgN4Fw";
+var products;
+
+axios({
+  method: "get",
+  url: "https://api-sandbox.duda.co/api/sites/multiscreen/28aa80cd8a194babadd11e78897c7d15/ecommerce/products",
+  auth: {
+    username: user,
+    password: password,
+  },
+})
+  .then(function (response) {
+    products = response.data.results;
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  });
+
 exports.getAllProducts = (req, res) => {
   res.status(200).json({
     status: "success",
     results: 0,
-    data: {},
+    data: products,
   });
 };
