82e023f89969a98a7666c13320b66f0ec0d19b48
YonatanShaham <yjonas83@gmail.com>
2023-02-06 14:36:51 +0200

    Flatten products list and add site name and direct link

--- controllers/productController.js
+++ controllers/productController.js
@@ -7,10 +7,10 @@ var sites = site.sites;
 
 var products = [];
 
-const loadProductsBySiteName = (siteName) => {
+const loadProductsBySiteName = (site) => {
   axios({
     method: "get",
-    url: `https://api-sandbox.duda.co/api/sites/multiscreen/${siteName}/ecommerce/products`,
+    url: `https://api-sandbox.duda.co/api/sites/multiscreen/${site.siteName}/ecommerce/products`,
     auth: {
       username: creds.user,
       password: creds.password,
@@ -18,10 +18,12 @@ const loadProductsBySiteName = (siteName) => {
   })
     .then(function (response) {
       const siteProducts = {
-        siteName: siteName,
+        siteName: site.siteName,
+        baseUrl: site.url,
         results: response.data.results.length,
         products: response.data.results,
       };
+      //console.log(siteProducts);
       products.push(siteProducts);
     })
     .catch(function (error) {
@@ -32,7 +34,7 @@ const loadProductsBySiteName = (siteName) => {
 
 const loadAllProducts = () => {
   sites.forEach((site) => {
-    loadProductsBySiteName(site.siteName);
+    loadProductsBySiteName(site);
   });
 };
 
@@ -42,10 +44,29 @@ const init = async () => {
 
 init();
 
-exports.getAllProducts = (req, res) => {
+exports.getRawProducts = (req, res) => {
   res.status(200).json({
     status: "success",
     results: products.length,
     data: products,
+  });
+};
+
+exports.getAllProducts = (req, res) => {
+  var list = [];
+  products.forEach((site) => {
+    site.products.forEach((product) => {
+      console.log(site);
+      var listItem = product;
+      listItem.siteName = site.siteName;
+      listItem.absolutPath =
+        site.baseUrl + "product/" + product.seo.product_url;
+      list.push(listItem);
+    });
+  });
+  res.status(200).json({
+    status: "success",
+    results: list.length,
+    data: list,
   });
 };
