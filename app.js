1c7c550c58ec59241209f5618a1bec76860aead8
YonatanShaham <yjonas83@gmail.com>
2023-02-06 13:56:02 +0200

    Pug setup

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require("express");
-
+const path = require("path");
 const productsRouter = require("./routes/productsRoutes");
 
 const app = express();
 
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.status(200).render("home");
+});
 app.use("/api/v1/products", productsRouter);
 
 module.exports = app;
